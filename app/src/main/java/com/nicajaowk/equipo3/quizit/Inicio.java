b282ef67ea7fcc74789f9cfed63201ea73ce0855
OwenCal <owencalzadillagough@gmail.com>
2015-01-22 18:07:04 +0100

    IntentReg

---
+++ app/src/main/java/com/nicajaowk/equipo3/quizit/Inicio.java
@@ -0,0 +1,44 @@
+package com.nicajaowk.equipo3.quizit;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+
+public class Inicio extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /*if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }else{
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            ActionBar actionBar = getActionBar();
+            actionBar.hide();
+        }*/
+
+
+        setContentView(R.layout.activity_inicio);
+
+    }
+
+    public void goToRegister(View buttonReg){
+        Intent j = new Intent(this, Register.class);
+        startActivity(j);
+    }
+}
