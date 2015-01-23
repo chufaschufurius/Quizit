1f44d9a7b207c3c9d5b42a2ca734695889c08aeb
Nicolas Alphonse <nicoalphonse@hotmail.com>
2015-01-23 17:59:11 +0100

    implemetnign register

--- app/src/main/java/com/nicajaowk/equipo3/quizit/Register.java
+++ app/src/main/java/com/nicajaowk/equipo3/quizit/Register.java
@@ -14,26 +14,33 @@ import android.widget.Toast;
 
 
 public class Register extends ActionBarActivity {
+    TextView email;
     TextView contrasena;
     TextView contrasena2;
-    final static String TAG = Register.class.getSimpleName();
-    static String mensaje = "contrasena incorrecta";
-    Toast toast;
+    static String mensajeContrasenaMala = "contrasena incorrecta";
+    static String mensajeCamposVacios = "Faltan campos por rellenar";
+    Toast toastContr;
+    Toast toastCamposVacios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        TextView email = (TextView) findViewById(R.id.editEmail);
+        email = (TextView) findViewById(R.id.editEmail);
         contrasena = (TextView) findViewById(R.id.editPass1);
         contrasena2 = (TextView) findViewById(R.id.editPass2);
-         toast = Toast.makeText(this, mensaje, Toast.LENGTH_LONG);
+        toastContr = Toast.makeText(this, mensajeContrasenaMala, Toast.LENGTH_LONG);
+        toastCamposVacios= Toast.makeText(this, mensajeCamposVacios, Toast.LENGTH_LONG);
+
+
         Button registrar = (Button) findViewById(R.id.buttonRegister);
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               if(!contrasena.getText().equals(contrasena2.getText()))
-                   toast.show();
+                if(email.getText().equals("") ||contrasena.equals("") || contrasena2.equals("") )
+                    toastCamposVacios.show();
+                else if(!contrasena.getText().equals(contrasena2.getText()))
+                    toastContr.show();
             }
         });
 
